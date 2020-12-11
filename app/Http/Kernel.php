3f283a400a2f6618d24839370250da47993ec06d
Dytlan <dytlanian@gmail.com>
2020-12-11 09:45:38 +0700

    feat: adding Middleware for is Admin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+        'is.admin' => \App\Http\Middleware\isAdminMiddleware::class,
     ];
 }

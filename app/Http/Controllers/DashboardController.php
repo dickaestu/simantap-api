fda8a1aaae72af71e9a94d150ca1c7f4683fea19
Dytlan <dytlanian@gmail.com>
2021-01-11 14:47:58 +0700

    refactor: refactor value dashboard grafik surat masuk dan keluar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,70 +13,37 @@ class DashboardController extends Controller
 {
     public function dashboard(Request $request)
     {
-        $start = $request->start_date . ' 00:00:01';
-        $end = $request->end_date . ' 23:59:00';
-
-        $DeferenceInDays = Carbon::parse($start)->diffInDays($end);
+        $start = Carbon::parse($request->start_date)->setTime(0,0,1);
+        $end = Carbon::parse($request->end_date)->setTime(23,59,59);
 
         $user = JWTAuth::user();
-        $seq = $user->bagian->seq;
 
         $surat_masuk_on_process = SuratMasuk::where('status', '<', 6)->count();
         $surat_masuk_done = SuratMasuk::where('status', 6)->count();
         $surat_keluar_on_process = SuratKeluar::where('status', '<', 3)->count();
         $surat_keluar_done = SuratKeluar::where('status', 3)->count();
 
-        $surat_masuk_monthly = SuratMasuk::select(
-            DB::raw('date(created_at) as created_date'),
-            DB::raw('count(id) as total_surat')
-        )
-            ->whereBetween('created_at', [$start, $end])
-            ->groupBy('created_at')->get()->pluck('total_surat', 'created_date')->all();
+        //Check Months
+        $diffInMonths = $start->diffInMonths($end);
 
-        $surat_keluar_monthly = SuratKeluar::select(
-            DB::raw('date(created_at) as created_date'),
-            DB::raw('count(id) as total_surat')
-        )
-            ->whereBetween('created_at', [$start, $end])
-            ->groupBy('created_at')->get()->pluck('total_surat', 'created_date')->all();
-        for ($i = Carbon::parse($start); $i <= Carbon::parse($end); $i->addDays()) {
-            if (array_key_exists($i->format('Y-m'), $surat_masuk_monthly)) {
-
-                $data[$i->format('Y-m')] = $surat_masuk_monthly[$i->format('Y-m')];
-            } else {
-
-                $data[$i->format('Y-m')] = 0;
-            }
-        }
-
-        //MonthList Surat Masuk
-        foreach ($data as $key => $dt) {
-            $obj = (object) array('date' => $key, 'value' => $dt);
-            $monthListSuratMasuk[] = $obj;
-        }
-
-        //MonthList Surat Keluar
-        foreach ($data as $key => $dt) {
-            $obj = (object) array('date' => $key, 'value' => $dt);
-            $monthListSuratKeluar[] = $obj;
+        for($i=0; $i< $diffInMonths;$i++){
+            $monthListMasuk[] = (object) [
+                'date' => Carbon::parse($start)->addMonth($i)->format('Y-m'),
+                'value' => 0,
+            ];
+            $monthListKeluar[] = (object) [
+                'date' => Carbon::parse($start)->addMonth($i)->format('Y-m'),
+                'value' => 0,
+            ];
         }
 
         //Count Surat Masuk
-        foreach ($surat_masuk_monthly as $key => $value) {
-            foreach ($monthListSuratMasuk as $month) {
-                if ($month->date == Carbon::parse($key)->format('Y-m')) {
-                    $month->value += $value;
-                }
-            }
+        foreach($monthListMasuk as $month){
+            $month->value = SuratMasuk::where('created_at', 'like', $month->date.'%')->count();
         }
-
         //Count Surat Keluar
-        foreach ($surat_keluar_monthly as $key => $value) {
-            foreach ($monthListSuratKeluar as $month) {
-                if ($month->date == Carbon::parse($key)->format('Y-m')) {
-                    $month->value += $value;
-                }
-            }
+        foreach($monthListKeluar as $month){
+            $month->value = SuratKeluar::where('created_at', 'like', $month->date.'%')->count();
         }
 
         return response()->json([
@@ -85,8 +52,8 @@ class DashboardController extends Controller
             'surat_masuk_done' => $surat_masuk_done,
             'surat_keluar_on_process' => $surat_keluar_on_process,
             'surat_keluar_done' => $surat_keluar_done,
-            'grafik_surat_masuk' => $monthListSuratMasuk,
-            'grafik_surat_keluar' => $monthListSuratKeluar,
+            'grafik_surat_masuk' => $monthListMasuk,
+            'grafik_surat_keluar' => $monthListKeluar,
         ], 200);
     }
 }

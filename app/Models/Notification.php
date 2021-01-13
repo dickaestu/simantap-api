da91b62ccac734af940825e4cae28d2e268f0b67
Dytlan <dytlanian@gmail.com>
2021-01-13 19:27:34 +0700

    feat: adding body and title in data field notification

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,7 +8,8 @@ class Notification extends Model
 {
     protected $fillable = ['user_id', 'read_at'];
 
-    public function notifable(){
+    public function notifable()
+    {
         return $this->morphTo();
     }
 
@@ -20,30 +21,32 @@ class Notification extends Model
         $fields = [
             'to' => $firebase['token'],
             'notification' => [
-                  'body' => $firebase['body'],
-                  'title' => $firebase['title']
-             ],
+                'body' => $firebase['body'],
+                'title' => $firebase['title']
+            ],
             'data' => [
-                'id' => $firebase['data']['id'], 
+                'body' => $firebase['body'],
+                'title' => $firebase['title'],
+                'id' => $firebase['data']['id'],
                 'type' => $firebase['data']['type']
             ],
         ];
 
-        $fields_encode = json_encode ( $fields );
+        $fields_encode = json_encode($fields);
         $headers = [
-            'Authorization:key='.$key_authorization,
+            'Authorization:key=' . $key_authorization,
             'Content-Type:application/json',
             'project_id' => $project_id
         ];
 
         $ch = curl_init();
-        curl_setopt( $ch, CURLOPT_URL, $url );
-        curl_setopt( $ch, CURLOPT_CUSTOMREQUEST,"POST");
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, $fields_encode );
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_encode);
 
-        $out = curl_exec( $ch );
-        curl_close( $ch );
+        $out = curl_exec($ch);
+        curl_close($ch);
     }
 }
